dd680c55dd70eed43a5d1b2d963a4fc67ebe2834
João Paulo Ferreira <jferreirav75@gmail.com>
2019-03-01 21:28:54 -0300

    debugar off

--- opiniao/settings.py
+++ opiniao/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&+qhsp+$1&^5)ugxvb$@effxr&+jwc7xfj_o#w-+bn#m+m&(g*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
